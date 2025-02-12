487ce96e4d3d9e7d799e7bd5d28197bdc92fefaf
gigas002 <gigas002@pm.me>
2025-02-12 22:51:11 +0900

    add wp on a 2nd monitor

--- dotfiles/dotlocal/bin/wallpaper.sh
+++ dotfiles/dotlocal/bin/wallpaper.sh
@@ -5,8 +5,9 @@ images_dir="${1:-$HOME/images/kamchoolya/}"
 # default to 15 minutes
 sleep_seconds="${2:-900}"
 
-# killall hyprpaper
-# hyprpaper &
+killall hyprpaper
+hyprpaper &
+hyprctl hyprpaper unload all
 
 swww query &> /dev/null
 if [ $? -ne 0 ] ; then
@@ -14,15 +15,19 @@ if [ $? -ne 0 ] ; then
 fi
 
 while true; do
+    # main output
+    main_output="eDP-1"
+    secondary_output="HDMI-A-1"
+
     # take random file from directory
     img_path=$(find $images_dir -type f | shuf -n 1)
     echo "$(date): $img_path" >> ~/.config/hypr/wallpaper.log
 
-    # hyprctl hyprpaper unload all
-    # hyprctl hyprpaper preload $img_path
-    # hyprctl hyprpaper wallpaper "eDP-1,$img_path"
+    swww img -o "$main_output" $img_path --transition-type grow --transition-fps 144 --transition-step 10
 
-    swww img $img_path --transition-type grow --transition-fps 144 --transition-step 10
+    # secondary output is transformed and it's not handled
+    # by swww properly
+    hyprctl hyprpaper reload "$secondary_output,tile:$img_path"
 
     sleep $sleep_seconds
 done
