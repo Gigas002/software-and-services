a88170b2b89fd342a586339631ba60b09a042d46
gigas002 <pushkinandrei@team-lab.com>
2026-05-12 17:08:38 +0900

    fix: comment-out cliphist and add clipvault

--- dotfiles/HOME/dotconfig/hypr/autostart.lua
+++ dotfiles/HOME/dotconfig/hypr/autostart.lua
@@ -11,8 +11,11 @@ local bar = "ashell"
 local bluetooth_applet = "blueman-applet"
 local network_applet = "nm-applet --indicator"
 local notification_daemon = "dunst"
+-- See: https://wiki.hypr.land/Useful-Utilities/Clipboard-Managers/#cliphist
 local clipboard_text = "wl-paste --type text --watch cliphist store"
 local clipboard_images = "wl-paste --type image --watch cliphist store"
+-- See: https://wiki.hypr.land/Useful-Utilities/Clipboard-Managers/#clipvault
+local clipvault = "wl-paste --watch clipvault store"
 
 -- for awww
 -- local wallpaper = "wallpaper.nu"
@@ -26,8 +29,8 @@ hl.on("hyprland.start", function()
     -- hl.exec_cmd(bluetooth_applet)
     hl.exec_cmd(network_applet)
     hl.exec_cmd(notification_daemon)
-    hl.exec_cmd(clipboard_text)
-    hl.exec_cmd(clipboard_images)
+    -- hl.exec_cmd(clipboard_text)
+    -- hl.exec_cmd(clipboard_images)
     hl.exec_cmd(wallpaper)
     hl.exec_cmd(idle)
 end)
